97792576a7b3a441e1dd3e3c106eb27b0854c221
siva <sivasebi4545@gmail.com>
2023-07-19 15:43:09 +0530

    countoddnumber

---
+++ main.js
@@ -0,0 +1,12 @@
+var arr=[10,27,35,40,30,60,82,99];
+var count=0;
+document.write("Given arry elements"+arr+"<br>");
+for(let i=0;i<arr.length;i++){
+	if(arr[i]%2!=0){
+		
+		Count++;
+	}
+	
+}
+        document.write("count the odd number in the array"+count);
+		document.write("<br>")
